18ae09dd0e56a9265b2f3b41a0d2e00ef7f72f30
Steve Agyeman <steve@Steves-MacBook-Air.local>
2022-03-06 19:06:04 -0500

    first commit

---
+++ db.js
@@ -0,0 +1,28 @@
+var sqlite3 = require("sqlite3");
+var mkdirp = require("mkdirp");
+var crypto = require("crypto");
+
+mkdirp.sync("var/db");
+
+var db = new sqlite3.Database("var/db/todos.db");
+
+db.serialize(function () {
+  // create the database schema for the todos app
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users ( \
+    username TEXT UNIQUE, \
+    hashed_password BLOB, \
+    salt BLOB \
+  )"
+  );
+
+  db.run(
+    "CREATE TABLE IF NOT EXISTS todos ( \
+    owner_id INTEGER NOT NULL, \
+    title TEXT NOT NULL, \
+    completed INTEGER \
+  )"
+  );
+});
+
+module.exports = db;
